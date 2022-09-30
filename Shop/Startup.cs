d7595106170967854eabfbb708c5421e2f96d27e
sv <sv@DESKTOP-0KR13D0>
2022-09-30 13:21:26 +0300

    Добавьте файлы проекта.

---
+++ Shop/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Data;
+using Shop.Data.interfaces;
+using Shop.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Shop {
+    public class Startup {
+
+
+        private IConfiguration _confSting;
+
+        //public Startup(IHostingEnvironment hostEnv)
+        //{
+        //    _confSting = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        //}
+        public Startup(IConfiguration configuration)
+        {
+            //_confSting = configuration;
+            _confSting = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("dbsettings.json").Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confSting.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IAllCars, CarRepository>();
+            services.AddTransient<ICarsCategory, CategoryRepository>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            using (var scope = app.ApplicationServices.CreateScope()) {
+                AppDBContent content = scope.ServiceProvider.GetRequiredService<AppDBContent>();
+                DBObjects.Initial(content);
+            }
+        }
+    }
+}
